5568a23eaff3c7d6276b3aa8de2a1d4bbbc72559
Sylvia  Ranjeva <sranjeva@Sylvias-MacBook-Air.local>
2020-05-08 16:21:40 -0500

    updates for regional forecasting

--- Forecasting/input_file_specification.R
+++ Forecasting/input_file_specification.R
@@ -4,7 +4,7 @@ rprocFile = '../Inference/rprocess_noise_non_hosp_deaths_region_contacts.c'
 nu_scales_file = '../Data/nu_scaling.csv'
 fraction_underreported_file = "../Data/frac_underreported.csv"  
 default_par_file = "../Parameters/parameter_values.csv"
-contact_filename = covid_get_path('../Data/IL_POMP_contacts_expanded.RData')
+contact_filename = '../Data/IL_POMP_contacts_expanded.RData'
 intervention_table_filename = "intervention_table.csv"
 
 ## Specify population filenames
