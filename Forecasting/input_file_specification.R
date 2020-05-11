ec64e11fe22c2ac5549e46ad44436b4228f1cf83
philarevalo <philip.a.arevalo@gmail.com>
2020-05-11 07:25:27 -0500

    Changed to new contacts and intervention file

--- Forecasting/input_file_specification.R
+++ Forecasting/input_file_specification.R
@@ -4,8 +4,8 @@ rprocFile = '../Inference/rprocess_noise_non_hosp_deaths_region_contacts.c'
 nu_scales_file = '../Data/nu_scaling.csv'
 fraction_underreported_file = "../Data/frac_underreported.csv"  
 default_par_file = "../Parameters/parameter_values.csv"
-contact_filename = '../Data/IL_POMP_contacts_expanded.RData'
-intervention_table_filename = "intervention_table.csv"
+contact_filename = '../Data/IL_metaregions_symmetric.RData'
+intervention_table_filename = "./intervention_table.csv"
 
 ## Specify population filenames
 population_filename_1= "../Data/IL_population_EMS_regions_1-2.csv"
