d3edb1d0df89971e1d04b732b8269b35355a82cb
Philip Arevalo <parevalo@midway2-login1.rcc.local>
2020-06-02 22:12:29 -0500

    Added scripts to run on different machine

---
+++ Inference/grid_search/pfilter_search_theta.R
@@ -0,0 +1,106 @@
+## Load packages and specify files 
+library(dplyr)
+library(pomp)
+library(ggplot2)
+library(magrittr)
+library(lubridate)
+library(MASS)
+library(reshape2)
+library(dplyr)
+library(foreach)
+
+select <- dplyr::select
+rename <- dplyr::rename
+summarize <- dplyr::summarise
+contains <- dplyr::contains
+
+args = commandArgs(trailingOnly=TRUE)
+
+root <- '../../'
+source(file.path(root, '_covid_root.R'))
+covid_set_root(root)
+
+default_par_file = './default_parameter_values.csv'
+deltaT = 0.1
+
+jobid_master = as.numeric(args[1])
+arrayid = as.numeric(args[2])
+output_dir = args[3]
+dmeasFile = args[4]
+data_filename = args[5]
+population_filename_1 = args[6]
+population_filename_2 = args[7]
+population_filename_3 = args[8]
+simstart = args[9]
+simend = args[10]
+min_data_time = args[11]
+intervention_start=args[12]
+num_cores=as.numeric(args[13])
+maxjobs=as.numeric(args[14])
+min_data_time_ICU = args[15]
+init_file=args[16]
+rprocFile=args[17]
+nu_scales_file=args[18]
+fraction_underreported_file=args[19]
+contact_filename=args[20]
+inference_file=args[21]
+t_ref=args[22]
+intervention_file=args[23]
+population_filename_4 = args[24]
+region_to_test=as.numeric(args[25])
+
+n_reps_pfilter=5
+n_particles_pfilter=3000
+
+design = read.csv('points_to_search.csv')
+
+num_points=nrow(design)
+print(head(design))
+source(covid_get_path(inference_file))
+
+source('set_up_covariates_and_data.R')
+
+
+if (jobid_master <= num_points){
+  for (name in names(design)){
+    pars[[name]] = design[jobid_master, name]
+  }
+  ## Make a pomp object for inference
+  print('Making pomp object')
+  pomp_object <- make_pomp_object_covid(
+      n_regions = pars$n_regions,
+      n_age_groups = n_age_groups,
+      input_params = pars,
+      delta_t = deltaT,
+      contacts=pomp_contacts,
+      population=population_list,
+      nu_scales=nu_scales,
+      beta_scales=beta_scales,
+      frac_underreported=fraction_underreported,
+      dmeasure_Csnippet = dmeasure_snippet,
+      rprocess_Csnippet = rprocess_snippet,
+      rinit_Csnippet = rinit_snippet,
+      data=data,
+      fitstart=simstart,
+      time_column='time',
+      obsnames=observed_names,
+      transformations=transformation,
+      inherit_parameters=FALSE
+      )
+
+
+  print('pomp object created, running pfilter')
+
+
+  ## Run inference and return dataframes for output
+  run_pfilter_and_output_result(n_reps_pfilter, 
+    n_particles_pfilter, 
+    jobid_master,
+    arrayid,
+    output_dir,
+    pomp_object,
+    dmeasFile,
+    simstart,
+    min_data_time,
+    intervention_start)
+}
---
+++ Inference/grid_search/run_grid_search_theta.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+unique_jobid=1010 # An identifier to keep track of runs
+task_id=1 # index of the point to sample, currently goes from 1-40000
+cases=./emr_linelist_fitting_data.csv # File with death and ICU data, DO NOT COMMIT TO REPO
+output_dir=results_region_${region_to_test}/ 
+
+# Shouldn't need to change anything below this point
+region_to_test=-1
+nu_scale_file=Data/nu_scaling.csv
+frac_underreport_file=Data/frac_underreported.csv
+measurement_model=Inference/dmeasure_hosp_non_hosp_ICU.c
+
+# Demographic information
+population1=Data/IL_population_north-central.csv
+population2=Data/IL_population_central.csv
+population3=Data/IL_population_northeast.csv
+population4=Data/IL_population_southern.csv
+
+init_file=Inference/initializer_non_hosp_deaths.c
+rproc_file=Inference/rprocess_calculate_phis.c
+contact_file=Data/IL_4metaregions_symmetric.RData
+inference_file=Inference/inference_functions.R
+intervention_file=Forecasting/intervention_table.csv
+
+t_ref=2020-01-14
+t0=2020-03-01
+tf=2020-06-01
+data_start=2020-03-16
+intervention_start=2020-03-16
+ICU_start=2020-04-07
+
+ncores=-1 # only necessary if parallelizing via slurm
+maxjobs=-1 # only necessary if parallelizing via slurm
+
+module load R/3.5.1
+Rscript pfilter_search_theta.R ${task_id} ${unique_jobid} ${output_dir} ${measurement_model} ${cases} ${population1} ${population2} ${population3} ${t0} ${tf} ${data_start} ${intervention_start} ${ncores} ${maxjobs} ${ICU_start} ${init_file} ${rproc_file} ${nu_scale_file} ${frac_underreport_file} ${contact_file} ${inference_file} ${t_ref} ${intervention_file} ${population4} ${region_to_test}
