1ab4ceb8609277ccc97af99b334ca0d960401cec
Philip Arevalo <parevalo@midway2-login2.rcc.local>
2020-06-12 15:32:50 -0500

    Updated with new parameters

--- Inference/grid_search_external/aggregate_points.R
+++ Inference/grid_search_external/aggregate_points.R
@@ -1,5 +1,6 @@
 library(dplyr)
-library(RSQLite)
+library(data.table)
+#library(RSQLite)
 select <- dplyr::select
 # Read in arguments
 args = commandArgs(trailingOnly=TRUE)
@@ -8,15 +9,23 @@ output_dir = args[1]
 # Aggregate points
 print('Aggregating points')
 
-output_filename <- paste0(output_dir, "consolidated.all.pfilter.db")
-conn <- dbConnect(SQLite(), output_filename)
+#output_filename <- paste0(output_dir, "consolidated.all.pfilter.db")
+#conn <- dbConnect(SQLite(), output_filename)
 
+#file_path = output_dir #Replace this with a file path to the directory storing the csv files 
+#setwd(file_path)
+#file_list <- list.files(pattern = "output_pfilter_.*.csv$")
+
+#for (f in file_list){
+#    df = read.csv(f)
+#    df = df %>% select(-one_of(names(df)[grepl('C_.*', names(df))])) # Exclude contact matrix parameters
+#    dbWriteTable(conn, "pfilter_results", df, append=T)
+#}
+
+output_filename <- paste0(output_dir, "consolidated.all.pfilter.csv")
 file_path = output_dir #Replace this with a file path to the directory storing the csv files 
 setwd(file_path)
 file_list <- list.files(pattern = "output_pfilter_.*.csv$")
-
-for (f in file_list){
-    df = read.csv(f)
-    df = df %>% select(-one_of(names(df)[grepl('C_.*', names(df))])) # Exclude contact matrix parameters
-    dbWriteTable(conn, "pfilter_results", df, append=T)
-}
+data <- rbindlist(lapply(file_list, fread))
+df_output <- as.data.frame(data) %>% arrange(desc(loglik)) 
+write.csv(df_output, file = paste0('../', output_filename))
